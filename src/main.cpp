ce3f5c0cf972e2835d3c9214097cd59d140c922a
Michał Kaźmierczak <ghmkazmierczak@gmail.com>
2024-11-09 21:28:54 +0100

    main shouldn't be used right now

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,5 @@
 #include "student.hpp"
 
 int main(){
-       Database database;
-
-        Student adam {
-        "Adam",
-        "Kowalski",
-        "ul. Warszawska 32, 62-200 Gniezno",
-        1234523,
-        "00242703584",
-        Gender::Male
-    };
-    Student adrian {
-        "Adrian",
-        "Kowalski",
-        "ul. Warszawska 32, 62-200 Gniezno",
-        1234523,
-        "00242703884",
-        Gender::Male
-    };
-    database.add(adam);
-    database.add(adrian);
-    std::string expected = "Adam Kowalski; ul. Warszawska 32, 62-200 Gniezno; 1234523; 00242703584; Male. \n"
-                           "Adrian Kowalski; ul. Warszawska 32, 62-200 Gniezno; 1234523; 00242703884; Male. \n";
-
-    auto found = database.findByLastName("Kowalski");
-    std::cout << found << std::endl;
-    std::cout << expected << std::endl;
-    return 0;
+   return 0;
 }
