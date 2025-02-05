6ca562e0eca714e649ce5238ede373a4edc3ceb6
Michał Kaźmierczak <ghmkazmierczak@gmail.com>
2025-02-05 17:04:45 +0100

    Upgrade main

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,60 @@
 #include <iostream>
 #include <limits>
 #include "database.hpp"
-#include "student.hpp"
-#include "person.hpp"
 #include "employee.hpp"
+#include "person.hpp"
+#include "student.hpp"
 
-std::shared_ptr<Person> makeStudent() {
-    std::string name, lastname, address, PESEL, gender;
-    int indexNumber;
+Gender strToGender(const std::string& genderStr) {
+    if (genderStr == "Male") {
+        return Gender::Male;
+    }
+    if (genderStr == "Female") {
+        return Gender::Female;
+    }
+    return Gender::Other;
+}
+
+void getPersonalData(std::string& name, std::string& lastname, std::string& adress, std::string& pesel, Gender& gender) {
+    std::string genderStr;
+
     std::cout << "Enter name: \n";
     std::cin >> name;
     std::cout << "Enter lastname: \n";
     std::cin >> lastname;
     std::cout << "Ender address: \n";
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::getline(std::cin, address);
-    std::cout << "Enter index number: \n";
-    std::cin >> indexNumber;
+    std::getline(std::cin, adress);
     std::cout << "Enter PESEL: \n";
-    std::cin >> PESEL;
+    std::cin >> pesel;
     std::cout << "Enter gender: \n";
-    std::cin >> gender;
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin >> genderStr;
 
-    Gender gen;
-    if (gender == "Male") {
-        gen = Gender::Male;
-    } else if (gender == "Female") {
-        gen = Gender::Female;
+    gender = strToGender(genderStr);
+}
+
+void makePerson(Database& database, int choice) {
+    std::string name, lastname, adress, pesel, genderStr;
+    int value;
+    Gender gender;
+
+    getPersonalData(name, lastname, adress, pesel, gender);
+
+    if (choice == 1) {
+        std::cout << "Enter student's index number: \n";
+        std::cin >> value;
+        database.addStudent(name, lastname, adress, pesel, value, gender);
+        return;
     } else {
-        gen = Gender::Other;
+        std::cout << "Enter employee's earnings: \n";
+        std::cin >> value;
+        database.addEmployee(name, lastname, adress, pesel, value, gender);
     }
-    Student s(name, lastname, address, indexNumber, PESEL, gen);
-    std::shared_ptr<Person> p = std::make_shared<Student>(s);
-    return p;
 }
 
 int main() {
     Database database;
-    int option, choice;
+    int choice;
     std::string value;
 
     Database::loadFromFile(database);
@@ -48,12 +64,14 @@ int main() {
     while (running) {
         std::cout << "Select option: " << std::endl;
         std::cout << "1. Add new student" << std::endl;
-        std::cout << "2. Display DataBase" << std::endl;
-        std::cout << "3. Find student" << std::endl;
-        std::cout << "4. Sort DataBase" << std::endl;
-        std::cout << "5. Remove student" << std::endl;
-        std::cout << "6. Save and exit program" << std::endl;
-        std::cin >> option;
+        std::cout << "2. Add new employee" << std::endl;
+        std::cout << "3. Display DataBase" << std::endl;
+        std::cout << "4. Find" << std::endl;
+        std::cout << "5. Sort DataBase" << std::endl;
+        std::cout << "6. Remove student" << std::endl;
+        std::cout << "7. Remove employee" << std::endl;
+        std::cout << "8. Save and exit program" << std::endl;
+        std::cin >> choice;
 
         if (std::cin.fail()) {
             std::cerr << "Error! Invalid input. Please enter a number between 1 and 6\n";
@@ -61,14 +79,15 @@ int main() {
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
 
-        switch (option) {
+        switch (choice) {
         case 1:
-            database.add(makeStudent());
-            break;
         case 2:
-            database.display();
+            makePerson(database, choice);
             break;
         case 3:
+            database.display();
+            break;
+        case 4:
             std::cout << "1. By lastname\n2. By PESEL\n";
             std::cin >> choice;
             switch (choice) {
@@ -77,10 +96,9 @@ int main() {
                 std::cin >> value;
                 try {
                     std::cout << database.findByLastName(value);
-                } catch (const std::runtime_error& e) {
-                    std::cout << "Student not found\n";
+                } catch (const std::runtime_error& error) {
+                    std::cout << "Not found\n";
                 }
-                std::cin.ignore();
                 break;
             case 2:
                 std::cout << "Enter PESEL: ";
@@ -88,39 +106,40 @@ int main() {
                 try {
                     std::cout << database.findByPESEL(value) << std::endl;
                 } catch (const std::runtime_error& e) {
-                    std::cout << "Student not found\n";
+                    std::cout << "Not found\n";
                 }
-                std::cin.ignore();
                 break;
             default:
                 std::cout << "Undefined value!";
                 break;
             }
             break;
-        case 4:
+        case 5:
             std::cout << "1. By lastname\n2. By PESEL\n";
             std::cin >> choice;
             switch (choice) {
             case 1:
                 database.sortByLastName();
-                std::cin.ignore();
                 break;
             case 2:
                 database.sortByPESEL();
-                std::cin.ignore();
                 break;
             default:
                 break;
             }
             break;
-        case 5:
+        case 6:
+            std::cout << "Enter employee's pesel: \n";
+            std::cin >> value;
+            database.removeEmployee(value);
+            break;
+        case 7:
             int indexNumber;
             std::cout << "Enter student's index number: \n";
             std::cin >> indexNumber;
             database.removeStudent(indexNumber);
-            std::cin.ignore();
             break;
-        case 6:
+        case 8:
             database.saveToFile();
             running = false;
             break;
